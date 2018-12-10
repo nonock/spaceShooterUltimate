411c228e1783d2a5451f32f2af09389383cfe2cd
Alexandre Lebegue <alexandre.lebegue@edu.esiee.fr>
2018-12-10 18:15:06 -0500

    add menu level

---
+++ Assets/Import/Main_menu_import/background_space.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class background_space : MonoBehaviour {
+
+	float alpha = 0.000f;
+	bool isZooming = false;
+	public Texture2D fadeTexture;
+
+
+float factor  = 0.0001F;
+	// Use this for initialization
+	void Start () {
+	 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localScale += new Vector3(factor,factor, 0);
+		if(isZooming){
+			alpha+= 0.01F;
+		}
+	}
+
+	public void zoom(){
+				factor = 0.01F;
+				isZooming = true;
+	}
+
+	void OnGUI()
+	{
+	GUI.color = new Color(1,1,1, alpha);
+	GUI.DrawTexture(new Rect(0,0,Screen.width,Screen.height), fadeTexture);
+	}
+}
---
+++ Assets/Import/Main_menu_import/ui_script.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ui_script : MonoBehaviour {
+
+	public background_space aScript;
+
+	public void StartGame() 
+	{
+	
+	AudioSource audioData = GetComponent<AudioSource>();
+	audioData.Play(0);
+	aScript.zoom();
+	print("hellooo");
+   // SceneManager.LoadScene("lvl");
+	}
+
+	public void QuitGame() 
+	{
+	  print("By");
+   // SceneManager.LoadScene("lvl");
+	}
+}
